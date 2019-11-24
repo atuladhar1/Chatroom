b84213aa0663a1e9ad6e085fcb3f932683067508
nameit83 <30509229+nameit83@users.noreply.github.com>
2019-11-23 23:16:43 -0600

    Almost Final if select doesnt work

--- Client.py
+++ Client.py
@@ -5,30 +5,31 @@ from threading import Thread
 # TODO send a tuple through the socket
 # set up an exit Function
 # IP and port addresses to connect to the server
-ip = "localhost"
-port = 8234
+ip = input('Please input IP to connect :')
+port = int(input('Please enter the port to connect :').strip())
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((ip,port))
+print(f'Connected to {ip} at port {port}')
 # userName=("userName",len(uname),uname)
 # initialize the socket
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((ip, port))
-recv = client.recv(1024).decode()
+recv = client.recv(4096).decode()
 print(recv)
 
 # send Username to server and wait for the acknowledgement
 userName= input("Enter the userName: ")
 client.send(bytes(userName,"utf8"))
-recv = client.recv(1024).decode()
+recv = client.recv(4096).decode()
 while (recv !=("Welcome")):
     print(recv)
     userName= input ("Please enter a new userName: ")
     client.send(bytes(userName,"utf8"))
-    recv=client.recv(1024).decode()
+    recv=client.recv(4096).decode()
 
 # recieve the current client list from server
-recv=client.recv(1024)
+recv=client.recv(4096)
 client_list=pickle.loads(recv)
 print(client_list[2])
-# functions to send message from user. Used as a concurrent thre`ad
+# functions to send message from user. Used as a concurrent thread
 def sendMessage():
     while True:
         message= input()
@@ -36,7 +37,7 @@ def sendMessage():
 # function to recieve message from user. Used as a concurrent thread.
 def recieveMessage():
     while True:
-        recv= client.recv(2048)
+        recv= client.recv(4096)
         message= pickle.loads(recv)
         print(message[2])
 # run the threads to recieve and send messages simultaneously
--- Server.py
+++ Server.py
@@ -1,15 +1,20 @@
 import socket
 import pickle
 from _thread import *
+import random
+
 ip = "localhost"
-port = 8234
+port = 8235
 client_list = {}
-# Add a client to the client list and broadcast the message to every other client
-# incomplete
+# Add a client to the client list and broadcast the message to every other client# incomplete
+
+
 # convert the message to be sent into at tuple
 def messager(message):
     tupl = ("Message",len(message),message)
     return tupl
+
+
 # convert the client list to string
 def listString():
     lost = list(client_list.keys())
@@ -18,14 +23,31 @@ def listString():
     lest = "Currently online members: "+ lister
     return lest
 
+
+def timely_broadcast(userName):
+    ts = random.randint(1,50)
+    if ts ==29 :
+        print (messager(listString())[2])
+        client_list[userName].send(pickle.dumps(messager(listString())))
+    
+
+def check_connection_status(client):
+    if client is False:
+        client.close()
+        print('Fuck you kumar')
+        
+
+
 # Send message to all the clients other than the one who send the message
 def sendMessage(connection,message):
         for client in client_list:
             if(client_list[client]!=connection):
                 client_list[client].send(pickle.dumps(message))
+
+
 # A singular thread function that will be used to multithread as required
-def createThread(connection, address):
-    connection.send(bytes("wassup", "utf8"))
+def createThread(connection,address):
+    connection.send(bytes("Welcome to the Server", "utf8"))
     # Take user name as unput
     userName = connection.recv(1024).decode()
     # Ask the Client for username that is not currently connected
@@ -37,12 +59,38 @@ def createThread(connection, address):
     client_list[userName]= connection
     for client in client_list:
         client_list[client].send(pickle.dumps(messager(listString())))
+
+
     # infinite loop receive a message and broadcast it to all the connected clients
     while (True):
-        recv = connection.recv(1024).decode()
-        message = userName+":" + recv
-        sendMessage(connection,messager(message))
-        print(message)
+        try:
+        
+            recv = connection.recv(1024).decode()
+            if recv is None:
+                print(f"connection closed to {username}")
+            message = userName+":" + recv
+            sendMessage(connection,messager(message))
+            timely_broadcast(userName)
+            print(message)
+            check_connection_status(client)
+
+        # except ConnectionResetError:
+        #     print (f"Connection was lost to {userName}")
+        #     exit()
+        
+        # except IOError:
+        #     print (f"{userName} hates you")
+        #     connection.close()        
+        #     exit()
+        
+        except:
+            continue
+        
+
+        
+
+
+
 
 # Core program that will call thred function and multithread
 # Values to be entered as required
@@ -50,7 +98,7 @@ def createThread(connection, address):
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     server.bind((ip, port))
     print(f"The server has been established at {ip} : {port}")
-    server.listen(2)
+    server.listen(5)
     while True:
         connection, address = server.accept()
         start_new_thread(createThread,(connection,address))
